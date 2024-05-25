72e1406c08f37a3b39e9fba1a1b7c9517a613fe0
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-25 15:35:39 +0800

    Create a.lua

---
+++ a.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Yourself and you said",
+    Desc = "Why'd do you hide and Survive me",
+    Reason = "Survive and escape the Trauma",
+    Image = "rbxassetid://11278624160"
+})
